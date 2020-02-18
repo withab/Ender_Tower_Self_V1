0b9151df1b8df77c2353d6c03fe2099b4ad7bd9a
withan <39221862+withab@users.noreply.github.com>
2020-02-18 22:55:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+ 
+let admin = "YOUR_ID_HERE"
+let prefix = "YOUR_PREFIX_HERE"
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+//TOWER
+//ON/OFF
+let isTower = false
+client.on("message", message => {
+    if(message.author.id == admin && message.content.startsWith(prefix + "tower")) {
+        let args = message.content.split(" ")[1].trim().toUpperCase()
+        if(!args) {
+            message.react("🔧")
+            message.delete()
+        } else {
+        if(args == "ON") {
+            message.react("✅")
+            isTower = true
+            message.delete(5000)
+        } else if(args == "OFF") {
+            message.react("❌")
+            isTower = false
+            message.delete(5000)
+        } else {
+            message.react("🔧")
+            message.delete(5000)
+        }
+    }
+    }
+})
+let random = (Math.random() * 3000 + 1000)
+//TOWER
+client.on("message", message => {
+    console.log(message)
+    if(message.author.id == "280726849842053120" && message.content.includes("⚡ *Energie restante : ") && isTower) {
+        
+        setTimeout(() => {
+            message.react("🔁")
+        }, random)
+    }
+})
+
+
+
+client.login("YOUR_TOKEN_HERE")
